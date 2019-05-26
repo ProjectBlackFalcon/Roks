5ad09aa68079016165dda202fa82824c20152780
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-05-26 19:06:30 -0400

    Absolutely godly documentation

--- training/TrainingGPT2.py
+++ training/TrainingGPT2.py
@@ -5,15 +5,21 @@ import torch.nn.functional as F
 from tqdm import trange
 from DataGenerator import get_disk_posts
 
+# GPT-2 tokenizer with byte-pair encoding
 enc = GPT2Tokenizer.from_pretrained("gpt2")
+
+# Pre-trained models with weights trained on WebText
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 model.eval()
 
+# Fetching data scraped from elsewhere
 posts = get_disk_posts()
 post = posts[0]
 
+# Using the negative log likelihood loss function
 criterion = nn.NLLLoss()
 
+# Using the OpenAI Adam implementation and setting up the model weights with it
 param_optimizer = list(model.named_parameters())
 no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
 optimizer_grouped_parameters = [
@@ -22,40 +28,65 @@ optimizer_grouped_parameters = [
 ]
 optimizer = OpenAIAdam(optimizer_grouped_parameters, lr=0.9)
 
+# Hyper-parameters
 batch_size = 1
 length = model.config.n_ctx // 2
 temperature = 1
 
 
 def train(model, target_tensor, criterion, optimizer):
+    """
+    Trains the model
+    :param model: Actual GPT-2 model
+    :param target_tensor: a list of tokens corresponding to the target document. Document should have been tokenified by
+    the GPT-2 tokenizer
+    :param criterion: The chosen loss function
+    :param optimizer: The chosen optimizer
+    :return: the loss value over this iteration
+    """
     if len(target_tensor) > 512:
         raise Exception("Document contains more than 512 tokens.")
 
+    # Initial parameters reset
     loss = 0
     optimizer.zero_grad()
 
+    # Iterate over every token
     for i in trange(1, len(target_tensor)):
+        # Define the part of the sentence which will be used as the context
         context_tokens = target_tensor[:i]
+        # Create a tensor and adapt it to the model accepted shape
         context = torch.tensor(context_tokens, dtype=torch.long).unsqueeze(0).repeat(batch_size, 1)
-        logits, past = model(context, past=None)
+        # Obtain the prediction from the model
+        logits, _ = model(context, past=None)
+        # Temperature dictates the certainty with which the model makes predictions
         logits = logits[:, -1, :] / temperature
+        # Gives a probability score to each predicted token; sum = 1
         log_probs = F.log_softmax(logits, dim=1)
+        # Cast the target to tensor so it can be used in the criterion
         singular_target_tensor = torch.tensor([target_tensor[i]])
 
+        # Calculate the loss using the probabilities compared to the expected output
         loss += criterion(log_probs, singular_target_tensor)
 
-    print("Loss backward...")
     loss.backward()
-    print("Optimizer step + zero_grad ...")
     optimizer.step()
 
     return loss.item() / len(target_tensor)
 
 
 def train_iterations(model, target_tensors, criterion, optimizer, print_every=1):
-
+    """
+    Trains the model with every document
+    :param model: Actual GPT-2 model
+    :param target_tensors: list of all documents that will be fed to the model
+    :param criterion: The chosen loss function
+    :param optimizer: The chosen optimizer
+    :param print_every: Every m iteration the average loss will be printed
+    :return:
+    """
     print_total_loss = 0
-
+    # Iterate through every example and print the loss when necessary
     for iteration in range(len(target_tensors)):
         print("{}/{}".format(iteration, len(target_tensors)))
         post = target_tensors[iteration]
